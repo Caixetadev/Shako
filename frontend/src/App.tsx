065ef1f6406f95a847e74514572853674e14eed0
ykwy404 <thekaway404@gmail.com>
2022-11-09 21:27:28 -0300

    feat: socket lost

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,30 +2,43 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import Login from './pages/auth/login'
 
-const ws = new WebSocket('ws://localhost:9000/ws')
+let ws = new WebSocket('ws://localhost:9000/ws')
 
 function App() {
   const [user, setUser] = useState({id: null});
+  let intervalConnect = setInterval(function() {
+    clearInterval(intervalConnect)
+  }, 1000);
   
   useEffect(() => {
-      ws.onopen = () => {
-        // on connecting, do nothing but log it to the console
-        console.log('connected')
+      const connect = () => {
+        ws = new WebSocket('ws://localhost:9000/ws')
+        ws.onopen = () => {
+          // on connecting, do nothing but log it to the console
+          console.log('connected')
+          clearInterval(intervalConnect)
+        }
+  
+        ws.onclose = () => {
+          console.log('disconnected')
+          clearInterval(intervalConnect)
+          intervalConnect = setInterval(function() {
+            //Try
+            console.log('Try connect again in 1 second')
+            connect();
+          }, 1000);
+        }
+  
+        ws.onerror = err => {
+          console.error(
+              "Socket encountered error: ",
+              "Closing socket"
+          );
+  
+          ws.close();
+      };
       }
-
-      ws.onclose = () => {
-        console.log('disconnected')
-      // automatically try to reconnect on connection loss
-      }
-
-      ws.onerror = err => {
-        console.error(
-            "Socket encountered error: ",
-            "Closing socket"
-        );
-
-        ws.close();
-    };
+      connect()
   }, []);
 
   const setLogged = (user: Object) => {
