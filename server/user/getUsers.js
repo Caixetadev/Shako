3f43cf6698d3a210f75c8b816a15e0bdd5121a6c
ykwy404 <thekaway404@gmail.com>
2023-01-04 15:32:55 -0300

    fix

--- server/user/getUsers.js
+++ server/user/getUsers.js
@@ -1,5 +1,14 @@
-function getOtherUsers(knex, currentUserId) {
-    return knex('users').where('id', '!=', currentUserId).map((user) => user.token = undefined);
+async function getOtherUsers(knex, currentUserId) {
+    const users_ = []
+    const users = await knex('users').where('id', '!=', currentUserId).then(function(rows) {
+        rows.map((user) => {
+            user.password = undefined
+            user.token = undefined
+            users_.push(user)
+            console.log(users_)
+        })
+    })
+    return users_
 }
 
 module.exports = {getOtherUsers}
